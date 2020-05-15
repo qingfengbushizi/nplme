346355871a998a903de6bf5bcb40621fb34894d4
qingfeng1234 <13861261326@163.com>
2020-05-15 14:16:08 +0800

    edit

--- App/testdemo/android/app/src/main/java/com/testdemo/MainApplication.java
+++ App/testdemo/android/app/src/main/java/com/testdemo/MainApplication.java
@@ -3,6 +3,8 @@ package com.testdemo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.daiyan.RNAlibcSdkPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import it.innove.BleManagerPackage;
 import com.beefe.picker.PickerViewPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAlibcSdkPackage(),
+            new RNDeviceInfo(),
             new BleManagerPackage(),
             new PickerViewPackage(),
             new RNGestureHandlerPackage()
--- App/testdemo/application/page/home/Home.js
+++ App/testdemo/application/page/home/Home.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity,Image ,TextInput} from 'react-native';
-
+import { View, Text, StyleSheet, FlatList, TouchableOpacity,Image ,TextInput,NativeModules} from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 import Picker from 'react-native-picker';
 import MapView from 'react-native-maps';
+import RNAlibcSdk from 'react-native-alibc-sdk';
+
+
 
 class Home extends Component {
 
@@ -30,6 +33,8 @@ class Home extends Component {
 
     render(){
         const {navigation} = this.props;
+        console.log(DeviceInfo.getUniqueID());
+        console.log(RNAlibcSdk);
         return(
             <TouchableOpacity onPress={()=>navigation.navigate('Test')}  style={{width:100,height:100,backgroundColor:'#ddd'}}>
                 <Text>werwrw</Text>
